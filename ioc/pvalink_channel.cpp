4a9e18bb21501e8f8c7d6030ec0b8dcb0fc21c28
Ivan Finch <ivan.finch@stfc.ac.uk>
2026-06-29 15:06:42 +0100

    Revert attempt to fix pvalink test failures

--- ioc/pvalink_channel.cpp
+++ ioc/pvalink_channel.cpp
@@ -413,6 +413,9 @@ void pvaLinkChannel::run()
             links_changed = false;
         }
 
+        update_seq++;
+        update_evt.signal();
+        log_debug_printf(_logger, "%s Sequence point %u\n", key.first.c_str(), update_seq);
     }
     // unlock link
 
@@ -426,13 +429,6 @@ void pvaLinkChannel::run()
     for(auto& trac : nonatomic_records) {
         ioc::DBLocker L(trac.prec);
         trac.scan();
-    }
-
-    {
-        Guard G(lock);
-        update_seq++;
-        update_evt.signal();
-        log_debug_printf(_logger, "%s Sequence point %u\n", key.first.c_str(), update_seq);
     }
 
     log_debug_printf(_logger, "Requeueing %s\n", key.first.c_str());
